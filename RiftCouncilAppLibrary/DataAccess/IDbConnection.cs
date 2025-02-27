9ac1cbb2f5189daefae0c2477d82c93f5fd7d97d
Your Name <your.email@example.com>
2025-02-27 10:51:09 +0100

    interface initialization

---
+++ RiftCouncilAppLibrary/DataAccess/IDbConnection.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+
+namespace RiftCouncilAppLibrary.DataAccess
+{
+    public interface IDbConnection
+    {
+        IMongoCollection<CategoryModel> CategoryCollection { get; }
+        string CategoryCollectionName { get; }
+        MongoClient Client { get; }
+        string Dbname { get; }
+        IMongoCollection<StatusModel> StatusCollection { get; }
+        string StatusCollectionName { get; }
+        IMongoCollection<SuggestionModel> SuggestionCollection { get; }
+        string SuggestionCollectionName { get; }
+        string UserCollectionName { get; }
+        IMongoCollection<UserModel> UserCollection { get; }
+    }
+}
