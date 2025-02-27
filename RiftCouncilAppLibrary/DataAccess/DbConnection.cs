a403434e5adf35b51f832f2dece2cef3fa231fc2
Your Name <your.email@example.com>
2025-02-27 10:10:22 +0100

    set up of DbConnection

--- RiftCouncilAppLibrary/DataAccess/DbConnection.cs
+++ RiftCouncilAppLibrary/DataAccess/DbConnection.cs
@@ -1,7 +1,39 @@
 ﻿
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
 namespace RiftCouncilAppLibrary.DataAccess;
 
 public class DbConnection
 {
+    private readonly IConfiguration _config;
+    private readonly IMongoDatabase _db;
+    private string _connectionId = "MongoDB";
+    public string Dbname { get; private set; }
+    public string CategoryCollectionName { get; private set; } = "categories";
+    public string StatusCollectionName { get; private set; } = "statuses";
+    public string UserCollectionName { get; private set; } = "users";
+    public string SuggestionCollectionName { get; private set; } = "suggestions";
+    public MongoClient Client { get; private set; }
+
+    public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
+    public IMongoCollection<StatusModel> StatusCollection { get; private set; }
+    public IMongoCollection<UserModel> UserCollections { get; private set; }
+    public IMongoCollection<SuggestionModel> SuggestionCollection { get; private set; }
+
+    public DbConnection(IConfiguration config)
+    {
+        _config = config;
+        Client = new MongoClient(_config.GetConnectionString(_connectionId));
+        Dbname = _config["DatabaseName"];
+        _db = Client.GetDatabase(Dbname);
+
+        CategoryCollection = _db.GetCollection<CategoryModel>(CategoryCollectionName);
+        StatusCollection = _db.GetCollection<StatusModel>(StatusCollectionName);
+        UserCollections = _db.GetCollection<UserModel>(UserCollectionName);
+        SuggestionCollection = _db.GetCollection<SuggestionModel>(SuggestionCollectionName);
+    }
 }
+
+
 
