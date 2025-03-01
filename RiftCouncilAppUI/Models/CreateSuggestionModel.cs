5b0654c67f875b4b4edbd5e3ca2f51ab46ed7d47
Bartłomiej Młynarski <bartek.mlynarski18@gmail.com>
2025-03-01 12:24:10 +0100

    SetUp CreateSuggestionMdel

---
+++ RiftCouncilAppUI/Models/CreateSuggestionModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RiftCouncilAppUI.Models
+{
+    public class CreateSuggestionModel
+    {
+        [Required]
+        [MaxLength(75, ErrorMessage = "Name is too long.")]
+        public string Suggestion { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Category is required.")]
+        [Display(Name = "Category")]
+        public string CategoryId { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Description is too long.")] 
+        public string Description { get; set; }
+
+    }
+}
